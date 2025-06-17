a41e81f437467829227aa4fd107a7d979dc0bd7f
Victor GS <victor.vgs2003@gmail.com>
2025-06-17 12:50:42 -0300

    Primeiro commit do projeto Spring Boot

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,69 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AgendaDeConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
+
+    public DadosDetalhamentoConsulta agendar(DadosAgendamentoConsulta dados){
+        if (!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("ID do paciente informado não existe.");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("ID do medico informado não existe.");
+        }
+
+        validadores.forEach(v -> v.validar(dados));
+
+        //var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var medico = escolherMedico(dados);
+
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
+        consultaRepository.save(consulta);
+
+        return new DadosDetalhamentoConsulta(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigatorio, quando o médico não for escolhido.");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
+    }
+}
---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
@@ -0,0 +1,10 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public interface ValidadorAgendamentoDeConsulta {
+
+    void validar(DadosAgendamentoConsulta dados);
+
+
+}
